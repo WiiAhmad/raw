04fea682ca3406777138f80abf9d9822b35c0d7b
Ahmad maulana yusuf <77580091+WiiAhmad@users.noreply.github.com>
2023-11-03 19:45:59 +0700

    do while with 000

--- raw.cpp
+++ raw.cpp
@@ -17,42 +17,43 @@ bool compareScores(const raw &a, const raw &b)
 int main()
 {
     raw data[100];
-    int n, score, totalFrequency = 0;
+    int n = 0, score, totalFrequency = 0;
 
-    cout << "input raw data" << endl;
-    cout << "========================" << endl;
-    cout << "Enter the number of data : ";
-    cin >> n;
-    cout << endl;
-
-    for (int i = 0; i < n; i++)
+    do
     {
-        cout << "Data " << i + 1 << endl;
-        cout << "Enter score : ";
+        cout << "Enter the score : ";
         cin >> score;
-
-        bool found = false;
-        for (int j = 0; j < i; j++)
+        if (score == 000)
         {
-            if (data[j].score == score)
+            break;
+        }
+        else
+        {
+            bool found = false;
+
+            for (int i = 0; i < n; i++)
             {
-                data[j].frequency++;
-                found = true;
-                break;
+                if (data[i].score == score)
+                {
+                    data[i].frequency++;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                data[n].score = score;
+                data[n].frequency = 1;
+                n++;
             }
         }
+    } while (score != 000);
 
-        if (!found)
-        {
-            data[i].score = score;
-            data[i].frequency = 1;
-        }
-    }
-
-    cout << endl;
     cout << "Raw Data" << endl;
     cout << "========================" << endl;
     sort(data, data + n, compareScores);
+
     for (int i = 0; i < n; i++)
     {
         if (data[i].score != 0 || data[i].frequency != 0)
@@ -68,35 +69,11 @@ int main()
     }
     cout << "Total Frequency : " << totalFrequency << endl;
 
-    // // Menginisialisasi smallestData dengan nilai dari elemen pertama dalam data
+    // Menginisialisasi smallestData dengan nilai dari elemen pertama dalam data
     float smallestData = data[0].score;
-    cout << "small sebelum" << smallestData << endl;
+    //cout << "small sebelum" << smallestData << endl;
     // Menginisialisasi largestData dengan nilai dari elemen pertama dalam data
-    float largestData = data[0].score;
-
-    // Mencari nilai terbesar dan terkecil dalam data
-    for (int i = 0; i < n; i++)
-    {
-        if (data[i].score != 0)
-        {
-            cout << "data" << data[i].score << endl;
-            if (smallestData < data[i].score)
-            {
-                smallestData = data[i].score;
-                cout << "small data" << smallestData << endl;
-                break;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        if (data[i].score > largestData)
-        {
-            largestData = data[i].score;
-        }
-    }
-
+    float largestData = data[n-1].score;
     // Menghitung rentang
     float range = largestData - smallestData;
 
